5bc7136776cee312b8c57605d9f3c5ead69d8061
sardorbeksidikov <sardorsidikov005@gmail.com>
2024-03-26 22:04:40 +0500

    sardorbek

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Students from './components/posts/Students';
+
+const App = () => {
+  return (
+    <div>
+      <Students />
+    </div>
+  )
+}
+
+export default App
